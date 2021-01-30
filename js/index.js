343cd47daba52ffee76d0154238902b16dd1b0d9
Patrick Stoebenau <pstoebenau@knights.ucf.edu>
2021-01-29 19:35:18 -0500

    Added portfolio section

--- js/index.js
+++ js/index.js
@@ -1,26 +1,44 @@
 const workSection = document.getElementById("work");
 
-const urls = [
-  "https://crownheritagelaw.com",
-  "https://bicbacboe.herokuapp.com/",
-  "https://polynomial-regression-nn.netlify.app/",
-  "https://4d-projection.netlify.app/"
+const websites = [
+  {
+    url: "https://crownheritagelaw.com",
+    img: "img/websites/crownheritagelaw.jpg"
+  },
+  {
+    url: "https://bicbacboe.herokuapp.com/",
+    img: "img/websites/bicbacboe.jpg"
+  },
+  {
+    url: "https://polynomial-regression-nn.netlify.app/",
+    img: "img/websites/polyregnn.jpg"
+  },
+  {
+    url: "https://4d-projection.netlify.app/",
+    img: "img/websites/4dproj.jpg"
+  }
 ];
 
-getImgFromUrl(urls[0]);
-
-async function getImgFromUrl(url) {
-  // Parameters for POST
-  const params = new URLSearchParams();
-  params.append("url", url);
-  params.append("device", "desktop");
-  params.append("cachelimit", "0");
-
-  const response = await fetch("https://www.screenshotmachine.com/capture.php", {
-    method: "POST",
-    body: params
-  })
-
-  console.log(response);
-  return response.link;
+function main() {
+  createWebsitePreviews();
 }
+
+function createWebsitePreviews() {
+  const template = document.getElementById("website-preview-template");
+  
+  for (let i in websites) {
+    const copy = template.content.cloneNode(true);
+    template.parentNode.appendChild(copy);
+  }
+
+  const websitePreviews = document.querySelectorAll(".website-preview");
+  for(let i in websites) {
+    websiteImage = websitePreviews[i].getElementsByTagName("img")[0];
+    websiteImage.src = websites[i].img;
+    websiteImage.addEventListener('click', () => {
+      window.open(websites[i].url);
+    });
+  }
+}
+
+main();
